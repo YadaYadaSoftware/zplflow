e6dcdf71e4160574edffed0ccbb9a580d7ae81c5
Tim <hounddog@gmail.com>
2022-11-18 10:03:35 -0500

    return $"{Codes.ScalableBitmappedFont}{this.Font.Code}{Codes.FieldBlockStart}{this.Width},{this.NumberOfLines}{Codes.FieldDataStart}{this.Text}{Codes.FieldDataEnd}";

--- ZplFlow/FieldBlock.cs
+++ ZplFlow/FieldBlock.cs
@@ -4,6 +4,6 @@ public record FieldBlock(FontBase Font, string Text, int Width, int NumberOfLine
 {
     public override string GetZpl()
     {
-        return $"{Codes.ScalableBitmappedFont}{this.Font.Code}{Codes.FieldBlockStart}{this.Width}{Codes.FieldDataStart}{this.Text}{Codes.FieldDataEnd}";
+        return $"{Codes.ScalableBitmappedFont}{this.Font.Code}{Codes.FieldBlockStart}{this.Width},{this.NumberOfLines}{Codes.FieldDataStart}{this.Text}{Codes.FieldDataEnd}";
     }
 }
